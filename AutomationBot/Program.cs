d935164821606c8fabe1fdb5ae62736d98d041a6
Tejas Firodiya <tejas.firodiya@arctechinfo.com>
2024-11-24 18:06:09 +0530

    Basic chromedriver implementation

--- AutomationBot/Program.cs
+++ AutomationBot/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationBot;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        using IWebDriver driver = new ChromeDriver(@"C:\Tools\chromedriver-win32\chromedriver-win32");
+        driver.Navigate().GoToUrl("https://www.google.com");
+        Console.WriteLine("Google opened successfully!");
+        driver.Quit();
+    }
+}
